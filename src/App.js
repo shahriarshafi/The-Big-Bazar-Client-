0f09c6e5e45c83627d3b3a695c932f7485171373
Shahriar Rubiat <shahriarshafi@gmail.com>
2021-04-06 16:44:47 +0600

    Commit

--- src/App.js
+++ src/App.js
@@ -45,10 +45,9 @@ function App() {
           <Route path="/home">
             <Home/>
           </Route>
-          <Route path="/admin">
-
+          <PrivateRoute path="/admin">
             <Admin/>
-          </Route>
+          </PrivateRoute>
           <Route path="/login">
             <Login/>
           </Route>
